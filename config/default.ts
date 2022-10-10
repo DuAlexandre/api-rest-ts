0153ab6134d9691e3a32d391186758416fbe08f4
DuAlexandre <luis.edu.alex@gmail.com>
2022-10-10 10:30:09 -0300

    Create dotenv

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,8 @@
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
 export default {
     port: 3000,
-    dbUri: "mongodb+srv://eduardo:root@cluster0.rb2karn.mongodb.net/?retryWrites=true&w=majority"
+    
+    dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.rb2karn.mongodb.net/?retryWrites=true&w=majority`
 }
